c6a6c4463ab747b5099ebd7e5f1b3fc33ee29a53
Esteban <eselman@hexacta.com>
2016-08-12 17:05:55 -0300

    se actualizo el doc de instalacion y se arreglo el test que fallaba.

--- source/backend/proyectosBA-DS/src/test/java/ar/gob/buenosaires/DSApplicationTests.java
+++ source/backend/proyectosBA-DS/src/test/java/ar/gob/buenosaires/DSApplicationTests.java
@@ -3,14 +3,16 @@ package ar.gob.buenosaires;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import ar.gob.buenosaires.config.SpringConfigDSTest;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DSApplication.class)
 @WebAppConfiguration
-@ActiveProfiles("dev")
+@ContextConfiguration(classes = SpringConfigDSTest.class)
 public class DSApplicationTests {
 
 	@Test
---
+++ source/backend/proyectosBA-DS/src/test/java/ar/gob/buenosaires/config/SpringConfigDSTest.java
@@ -0,0 +1,8 @@
+package ar.gob.buenosaires.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfigDSTest {
+
+}
