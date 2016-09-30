3759a497715b006e5c90f40c60755e48fa84a861
Esteban <eselman@hexacta.com>
2016-09-30 12:26:22 -0300

    se sacaron los permisos de acceso a la vista, para que pueda ser consultada de froma remoto.

--- source/configuracion/database/3-CREATE_exportacion_proyectos_view.sql
+++ source/configuracion/database/3-CREATE_exportacion_proyectos_view.sql
@@ -11,35 +11,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 -- Dumping structure for view proyectos_ba_generated.exportacion_proyectos_view
--- Creating temporary table to overcome VIEW dependency errors
-CREATE TABLE `exportacion_proyectos_view` (
-	`idProyecto` INT(10) NOT NULL,
-	`nombreJurisidiccion` VARCHAR(512) NOT NULL COLLATE 'latin1_swedish_ci',
-	`nombreProyecto` VARCHAR(512) NOT NULL COLLATE 'latin1_swedish_ci',
-	`estado` VARCHAR(50) NULL COLLATE 'latin1_swedish_ci',
-	`objetivoEstrategico` VARCHAR(512) NOT NULL COLLATE 'latin1_swedish_ci',
-	`objetivoOperativo` VARCHAR(512) NOT NULL COLLATE 'latin1_swedish_ci',
-	`descripcionProyecto` TEXT NULL COLLATE 'latin1_swedish_ci',
-	`responsable` VARCHAR(512) NULL COLLATE 'latin1_swedish_ci',
-	`area` VARCHAR(256) NULL COLLATE 'latin1_swedish_ci',
-	`organismosCorresponsables` VARCHAR(512) NULL COLLATE 'latin1_swedish_ci',
-	`fechaInicio` DATE NOT NULL,
-	`fechaFin` DATE NULL,
-	`tipoProyecto` VARCHAR(11) NULL COLLATE 'latin1_swedish_ci',
-	`implicaCambioLegislativo` TINYINT(4) NULL,
-	`prioridadJurisdiccional` VARCHAR(50) NULL COLLATE 'latin1_swedish_ci',
-	`meta` DECIMAL(22,2) NULL,
-	`unidadMeta` VARCHAR(512) NULL COLLATE 'latin1_swedish_ci',
-	`cantidadPoblacionImpactada` BIGINT(20) NULL,
-	`prioridadJefatura` BINARY(0) NULL,
-	`estadoAprobacion` BINARY(0) NULL
-) ENGINE=MyISAM;
-
-
--- Dumping structure for view proyectos_ba_generated.exportacion_proyectos_view
 -- Removing temporary table and create final VIEW structure
 DROP TABLE IF EXISTS `exportacion_proyectos_view`;
-CREATE ALGORITHM=UNDEFINED DEFINER=`admin`@`localhost` VIEW `exportacion_proyectos_view` AS SELECT DISTINCT p.idProyecto "idProyecto", j.nombre "nombreJurisidiccion", p.nombre "nombreProyecto", 
+CREATE VIEW `exportacion_proyectos_view` AS SELECT DISTINCT p.idProyecto "idProyecto", j.nombre "nombreJurisidiccion", p.nombre "nombreProyecto", 
 			  p.estado, oj.nombre "objetivoEstrategico", oop.nombre "objetivoOperativo", p.descripcion "descripcionProyecto", p.liderProyecto "responsable",
 			   a.nombre "area", p.organismosCorresponsables "organismosCorresponsables", p.fechaInicio "fechaInicio",
 			  p.fechaFin "fechaFin", p.tipoProyecto "tipoProyecto", p.cambioLegislativo "implicaCambioLegislativo",
